c693e67ac5b056f2cc4ad0344f32d80a653d4010
Rob Irving <rob.irving@gmail.com>
2026-05-02 12:36:34 -0400

    Delete this file
    it's name is too long for windows

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Multiday_Events/Multiday_Event/__snapshots__/BarTest__test_render_with_multiday_event_does_not_start_this_week_does_not_end_this_week_spans_multiple_weeks__1.php
+++
@@ -1,7 +0,0 @@
-<?php return '<div class="tribe-events-calendar-month__multiday-event-bar">
-	<div class="tribe-events-calendar-month__multiday-event-bar-inner">
-				<div class="tribe-events-calendar-month__multiday-event-bar-title tribe-common-h8">
-	Single Event 1</div>
-	</div>
-</div>
-';
